ed38094a61a2fe822c2c619a15c0b1ddba3c9b3f
erratika <mmarc.96@gmail.com>
2026-04-16 11:46:34 +0100

    init

---
+++ src/calendar/CalendarAnayear.ts
@@ -0,0 +1,30 @@
+export const calendarAnayear = {
+  id: "anayear-calendar",
+
+  yearLength: 360,
+
+  months: [
+    { name: "Newlight", days: 30 },
+    { name: "Embermoon", days: 30 },
+    { name: "Bloomwake", days: 30 },
+    { name: "Suncrest", days: 30 },
+    { name: "Ardshan", days: 30 },
+    { name: "Mooncrest", days: 30 },
+    { name: "Amberalt", days: 30 },
+    { name: "Harvestend", days: 30 },
+    { name: "Shadoward", days: 30 },
+    { name: "Frostgale", days: 30 },
+    { name: "Snowdrift", days: 30 },
+    { name: "Frostalt", days: 30 },
+  ],
+
+  weekdays: [
+    "Muinsdan",
+    "Taensdan",
+    "Vindesdan",
+    "Thrunsdan",
+    "Freindan"
+  ],
+
+  festivals: []
+};
